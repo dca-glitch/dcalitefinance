ac5c34c51af507c6f639cd7be6c883fdb8650e74
dca-glitch <piotrpakula360@gmail.com>
2026-06-10 21:27:08 +0800

    Add HTTP server bootstrap

---
+++ src/server.ts
@@ -0,0 +1,39 @@
+import { createServer } from 'node:http';
+import { createApp } from './app';
+import { env } from './config/env';
+import { logger } from './config/logger';
+import { prisma } from './config/prisma';
+
+const app = createApp();
+const server = createServer(app);
+
+server.listen(env.PORT, () => {
+  logger.info({ port: env.PORT, nodeEnv: env.NODE_ENV }, 'DCA Books Lite API started');
+});
+
+async function shutdown(signal: NodeJS.Signals): Promise<void> {
+  logger.info({ signal }, 'Shutdown signal received');
+
+  server.close(async (error) => {
+    if (error) {
+      logger.error({ err: error }, 'HTTP server shutdown failed');
+      process.exit(1);
+    }
+
+    await prisma.$disconnect();
+    logger.info('Shutdown complete');
+    process.exit(0);
+  });
+}
+
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
+
+process.on('unhandledRejection', (reason) => {
+  logger.error({ reason }, 'Unhandled promise rejection');
+});
+
+process.on('uncaughtException', (error) => {
+  logger.fatal({ err: error }, 'Uncaught exception');
+  process.exit(1);
+});
