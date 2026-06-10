82d7b89bc38b9d9741495f2ea8f9d90dce0b92a9
dca-glitch <piotrpakula360@gmail.com>
2026-06-10 21:26:48 +0800

    Add Express app

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import cors from 'cors';
+import express from 'express';
+import helmet from 'helmet';
+import { env } from './config/env';
+import { healthRoutes } from './routes/health.routes';
+import { requestIdMiddleware } from './middlewares/requestId.middleware';
+import { httpLoggerMiddleware } from './middlewares/httpLogger.middleware';
+import { notFoundMiddleware } from './middlewares/notFound.middleware';
+import { errorMiddleware } from './middlewares/error.middleware';
+
+export function createApp(): express.Express {
+  const app = express();
+
+  app.disable('x-powered-by');
+  app.use(helmet());
+  app.use(cors({ origin: env.CORS_ORIGIN }));
+  app.use(express.json({ limit: '1mb' }));
+  app.use(requestIdMiddleware);
+  app.use(httpLoggerMiddleware);
+  app.use('/api/v1/health', healthRoutes);
+  app.use(notFoundMiddleware);
+  app.use(errorMiddleware);
+
+  return app;
+}
