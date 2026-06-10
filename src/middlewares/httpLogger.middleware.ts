2d047ec8eab87a8851cfb8557d62db9d67667b4f
dca-glitch <piotrpakula360@gmail.com>
2026-06-10 21:31:51 +0800

    Make request id logging type-safe

--- src/middlewares/httpLogger.middleware.ts
+++ src/middlewares/httpLogger.middleware.ts
@@ -1,9 +1,10 @@
+import type { Request } from 'express';
 import pinoHttp from 'pino-http';
 import { logger } from '../config/logger';
 
 export const httpLoggerMiddleware = pinoHttp({
   logger,
   customProps: (request) => ({
-    requestId: request.requestId,
+    requestId: (request as Request).requestId,
   }),
 });
